09e696bac4068d637f5167ff865955a49a4c1b6e
Kouhei Maeda <mkouhei@palmtb.net>
2014-08-04 22:13:43 +0900

    Added unit test of cleanDirs().
    Signed-off-by: Kouhei Maeda <mkouhei@palmtb.net>

--- utils_test.go
+++ utils_test.go
@@ -6,9 +6,17 @@ import (
 )
 
 func TestWorkDirPath(t *testing.T) {
-	os.Setenv("WORKSPACE", "/tmp")
+	os.Setenv("WORKSPACE", "/tmp/foo")
+	os.Mkdir("/tmp/foo", 0600)
+	os.Mkdir("/tmp/bar", 0600)
 	wd := workDirpath()
-	if wd != "/tmp" {
+	if wd != "/tmp/foo" {
 		t.Fatal(wd)
 	}
+
+	c := &config{}
+	c.TempDirpath = "/tmp/foo"
+	c.ResultsDirpath = "/tmp/bar"
+
+	c.cleanDirs()
 }
